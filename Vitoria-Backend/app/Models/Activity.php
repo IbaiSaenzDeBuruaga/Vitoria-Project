ea2f2055de1dc97350c504b3ab326e673610ec60
JasonEgibide <jasonalejandro.varas@ikasle.egibide.com>
2025-02-11 10:32:36 +0100

    ralaciones en modelos

--- Vitoria-Backend/app/Models/Activity.php
+++ Vitoria-Backend/app/Models/Activity.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Activity extends Model
 {
-    //
+    protected $fillable = ['nombre', 'imagen'];
+
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+
+    public function users(){
+        return $this->belongsToMany(User::class);
+    }
+
+    public function centros_civicos(){
+        return $this->belongsToMany(CentroCivico::class)
+            ->using(ActivityCentro::class)
+            ->withPivot('fecha', 'horario');
+    }
 }
--- Vitoria-Backend/app/Models/CentroCivico.php
+++ Vitoria-Backend/app/Models/CentroCivico.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class CentroCivico extends Model
 {
-    //
+    protected $fillable = ['nombre', 'direccion'];
+
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+
+    public function actividades()
+    {
+        return $this->belongsToMany(Activity::class)
+            ->using(ActivityCentro::class)
+            ->withPivot('fecha', 'horario');
+    }
 }
