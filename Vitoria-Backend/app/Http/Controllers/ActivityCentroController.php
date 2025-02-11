408b3fca5747cecb148c03a84e8ccfbeb42742c9
JasonEgibide <jasonalejandro.varas@ikasle.egibide.org>
2025-02-11 09:56:40 +0100

    migrationa, models y controllers creados

---
+++ Vitoria-Backend/app/Http/Controllers/ActivityCentroController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ActivityCentro;
+use Illuminate\Http\Request;
+
+class ActivityCentroController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ActivityCentro $activityCentro)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ActivityCentro $activityCentro)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, ActivityCentro $activityCentro)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ActivityCentro $activityCentro)
+    {
+        //
+    }
+}
---
+++ Vitoria-Backend/app/Http/Controllers/CentroCivicoController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CentroCivico;
+use Illuminate\Http\Request;
+
+class CentroCivicoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CentroCivico $centroCivico)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(CentroCivico $centroCivico)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, CentroCivico $centroCivico)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CentroCivico $centroCivico)
+    {
+        //
+    }
+}
